88a042070e42ab95d71369901f57688de3278d87
Ismail Ismailov <ismail2ov@gmail.com>
2018-05-21 23:37:16 +0200

    Move CSV file to test resources folder

--- src/test/java/es/develex/RatesCalculatorShould.java
+++ src/test/java/es/develex/RatesCalculatorShould.java
@@ -12,6 +12,7 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
 
 public class RatesCalculatorShould {
+    private static final String TEST_CSV_FILE_PATH = System.getProperty("user.dir") + "\\src\\test\\java\\resources\\market.csv";
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 
@@ -24,14 +25,14 @@ public class RatesCalculatorShould {
 
     @Test
     public void when_requested_amount_is_1000_then_rate_is_7() {
-        String[] args = new String[]{"market.csv", "1000"};
+        String[] args = new String[]{TEST_CSV_FILE_PATH, "1000"};
         Main.main(args);
         assertThat(outContent.toString(), containsString("Rate: 7,0%"));
     }
 
     @Test
     public void test_when_requested_amount_is_1500() {
-        String[] args = new String[]{"market.csv", "1500"};
+        String[] args = new String[]{TEST_CSV_FILE_PATH, "1500"};
         Main.main(args);
         String expected = "Requested amount: £1500 \nRate: 7,1% \nMonthly repayment: £46,26 \nTotal repayment: £1665,39 \n";
         assertEquals(expected, outContent.toString());
@@ -39,7 +40,7 @@ public class RatesCalculatorShould {
 
     @Test
     public void test_when_requested_amount_is_15000() {
-        String[] args = new String[]{"market.csv", "15000"};
+        String[] args = new String[]{TEST_CSV_FILE_PATH, "15000"};
         Main.main(args);
         assertThat(outContent.toString(), containsString("Is not possible to provide a quote at that time"));
     }
